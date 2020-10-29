d7d83428c30b628c76a4dda17d933aa0ea65149f
Francesca <fraxrusso@gmail.com>
2020-10-29 10:22:29 +0100

    Initial Commit

---
+++ iDioms/iDiomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDiomsApp.swift
+//  iDioms
+//
+//  Created by Francesca Russo on 29/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct iDiomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
